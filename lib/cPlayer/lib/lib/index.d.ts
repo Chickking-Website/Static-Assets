a8e5d9cefbac26eb31d28a8391036577094d9bed
Chickking <me@chickger.pw>
2017-08-20 14:46:53 +0800

    Auto commit

---
+++ lib/cPlayer/lib/lib/index.d.ts
@@ -0,0 +1,43 @@
+/// <reference types="node" />
+import { IAudioItem, Iplaylist } from './interfaces';
+import { EventEmitter } from 'events';
+import cplayerView, { ICplayerViewOption } from './view';
+export interface ICplayerOption {
+    playlist?: Iplaylist;
+    playmode?: string;
+    volume?: number;
+    point?: number;
+    autoplay?: boolean;
+}
+export default class cplayer extends EventEmitter {
+    private __paused;
+    view: cplayerView;
+    audioElement: HTMLAudioElement;
+    private playmode;
+    private playmodeName;
+    mode: string;
+    volume: number;
+    readonly playlist: IAudioItem[];
+    readonly nowplay: IAudioItem;
+    readonly nowplaypoint: number;
+    readonly played: boolean;
+    readonly paused: boolean;
+    constructor(options: ICplayerOption & ICplayerViewOption);
+    private initializeEventEmitter();
+    private eventHandlers;
+    private isPlaying();
+    openAudio(audio?: IAudioItem): void;
+    toggleMode(): void;
+    setMode(playmode: string): void;
+    getMode(): string;
+    play(Forced?: boolean): void;
+    pause(Forced?: boolean): void;
+    to(id: number): void;
+    next(): void;
+    prev(): void;
+    togglePlayState(): void;
+    add(item: IAudioItem): void;
+    remove(item: IAudioItem): void;
+    setVolume(volume: number): void;
+    destroy(): void;
+}
